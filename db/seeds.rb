fda2b37640ea57930240cc3d3657509ff848d7d7
Max Bugel <bugelmax@gmail.com>
2020-05-21 15:30:49 +0300

    Implemented validation for blog, skills and portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
 10.times do |blog|
-  Blog.create!(
-    title: "My blog #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit voluptatum illum placeat animi, distinctio nesciunt quibusdam beatae possimus consectetur at, eaque ea, impedit modi vero natus dignissimos odit nisi quo."
-  )
+	Blog.create!(
+		title: "My blog #{blog}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit voluptatum illum placeat animi, distinctio nesciunt quibusdam beatae possimus consectetur at, eaque ea, impedit modi vero natus dignissimos odit nisi quo."
+	)
 end
 
 puts '10 blog posts created'
 
 5.times do |skill|
-  Skill.create!(
-    title: "Rails #{skill}",
-    percent_utilized: 13
-  )
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 13
+	)
 end
 
 puts '5 skills created'
 
 9.times do |portfolio_item|
-  Portfolio.create!(
-    title: "Portfolio title #{portfolio_item}",
-    subtitle: "My great service",
-    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, libero recusandae fugiat, animi ea ipsum quia doloribus aspernatur saepe veniam sequi nobis voluptas neque excepturi qui. Dicta ad ex inventore.",
-    main_image: "http://placekitten.com/g/500/700",
-    thumb_image: "http://placekitten.com/g/200/300"
-  )
+	Portfolio.create!(
+		title: "Portfolio title #{portfolio_item}",
+		subtitle: "My great service",
+		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, libero recusandae fugiat, animi ea ipsum quia doloribus aspernatur saepe veniam sequi nobis voluptas neque excepturi qui. Dicta ad ex inventore.",
+		main_image: "http://placekitten.com/g/500/700",
+		thumb_image: "http://placekitten.com/g/200/300"
+	)
 end
 
 puts '9 portfolio item created'
