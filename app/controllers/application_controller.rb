f27202e842ffbecb4f1d412e171444ed3b1e24b8
Max Bugel <bugelmax@gmail.com>
2020-06-02 11:36:15 +0300

    Added method for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = PortfolioViewTool::Renderer.copyright 'Max Buhel', 'All rights reserved'
+  end
+end
+
+module PortfolioViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <strong>#{name}</strong> #{msg}".html_safe
+      
+    end
+  end
 end
