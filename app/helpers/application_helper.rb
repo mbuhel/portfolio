6505df8404f670fbd4cabc01970919f32fa90360
Max Bugel <bugelmax@gmail.com>
2020-06-16 14:44:15 +0300

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,4 +57,16 @@ module ApplicationHelper
     "active" if current_page? path
     
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:notice] || flash[:error])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Max Buhel Portfolio", sticky: false)
+  end
 end
